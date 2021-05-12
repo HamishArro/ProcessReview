cf4e5b95fada2d740b048f6dabacfb673a75b72c
Hamish <hamisharro@icloud.com>
2021-05-12 18:37:45 +0100

    add logic for splitting and making equation

--- ProcessReview-1/main.swift
+++ ProcessReview-1/main.swift
@@ -8,5 +8,17 @@
 import Foundation
 
 func evaluate(_ input: String) -> (String, Int) {
-    return (input, 1)
+    var components = input.split(separator: " ")
+    var result: Int
+    if components.count > 1 {
+        switch components[1] {
+        case "+": result = Int(components[0]) + Int(components[2])
+        case "-": result = Int(components[0]) - Int(components[2])
+        case "*": result = Int(components[0]) * Int(components[2])
+        case "/": result = Int(components[0]) / Int(components[2])
+        default: print(components)
+        }
+        return (input, result)
+    } else {return (input, Int(input))}
+    
 }
--- ProcessReviewTests/ProcessReviewTests.swift
+++ ProcessReviewTests/ProcessReviewTests.swift
@@ -10,20 +10,18 @@ import XCTest
 
 class ProcessReviewTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testWhenOneValueIsEntered() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         let result = evaluate("1")
         XCTAssertEqual(result.1, 1)
         XCTAssertEqual(result.0, "1")
+    }
+    
+    func testWhenSimpleInput() {
+        let result = evaluate("12")
+        XCTAssertEqual(result.1, 12)
+        XCTAssertEqual(result.0, "12")
     }
 
 }
