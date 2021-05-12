2e99a18cc4d479918c46ef60d503707d4a5ee9ba
Hamish <hamisharro@icloud.com>
2021-05-12 15:14:54 +0100

    plan method

--- ProcessReviewTests/ProcessReviewTests.swift
+++ ProcessReviewTests/ProcessReviewTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import ProcessReview_1
 
 class ProcessReviewTests: XCTestCase {
 
@@ -17,16 +18,10 @@ class ProcessReviewTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testWhenOneValueIsEntered {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+        
     }
 
 }
--- README.md
+++ README.md
@@ -3,4 +3,30 @@
 
 ## Planning
 
+### String Calculator
+
+making math papers easer to read, scanning the string for sums
+extracting the sums as strings
+evaluates the sum
+outputs the sum string with reslut.
+
+just 2 terms
+
+evaluate("1 + 1")
+
+Input : "1 + 1"
+output : ["1 + 1", 2]
+
+handles
+- addition
+- subtraction
+- mutliplcation
+- division
+- BIDMAS (not a priority)
+
+edge case
+- the result will be an int
+- will always be an int
+- shouldnt be any characters in the sring, if there is rase a math error. "Input must be a vaild mathimatical string seperated by spaces"
+- just a number will not raise errors
 
