66b3133c0cd2216b486fc8bb0fba138ec83f7b7e
Hamish <hamisharro@icloud.com>
2021-05-14 16:41:24 +0100

    make failing test for invalid testing

--- ProcessReviewTests/ProcessReviewTests.swift
+++ ProcessReviewTests/ProcessReviewTests.swift
@@ -13,6 +13,26 @@ class ProcessReviewTests: XCTestCase {
     func testWhenWrongSymbol() throws {
         XCTAssertThrowsError(try evaluate("1 & 1")) // just checks to see if an error is thrown & below checks the type of error thrown
         XCTAssertThrowsError(try evaluate("1 % 1"), "evaluate() should throw error when inccorect input", { (errorThrown) in
+            XCTAssertEqual(errorThrown as? EvaluateError, EvaluateError.invalidSymbol)
+        })
+    }
+    
+    func testWhenLetterInString() throws {
+        XCTAssertThrowsError(try evaluate("1a + 1b"))
+        XCTAssertThrowsError(try evaluate("1 - 1b"), "evaluate() should throw error when inccorect input", { (errorThrown) in
+            XCTAssertEqual(errorThrown as? EvaluateError, EvaluateError.invalidInput)
+        })
+    }
+    
+    func testTwoInputs() throws {
+        let result = try evaluate("1 +")
+        XCTAssertEqual(result.1, 1)
+        XCTAssertEqual(result.0, "1 +")
+    }
+    
+    func testFiveInputs() throws {
+        XCTAssertThrowsError(try evaluate("1 + 1 + 4"))
+        XCTAssertThrowsError(try evaluate("1 + 1 + 4"), "evaluate() should throw error when inccorect input", { (errorThrown) in
             XCTAssertEqual(errorThrown as? EvaluateError, EvaluateError.invalidInput)
         })
     }
